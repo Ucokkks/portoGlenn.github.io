dc0f1a72168460f098707a2b71f7492323f5f948
Ucokkks <glenmartua@gmail.com>
2024-04-29 13:01:35 +0700

    new feature

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ const dynamicText = document.querySelector(".text-span");
 const words = ["Pelajar", "UI/UX", "Calon Mantu Idaman", "Full stack developer"];
 
 
-
-
-
-// Variables to track the position and deletion status of the word
 let wordIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
@@ -17,15 +13,12 @@ const typeEffect = () => {
   dynamicText.classList.add("stop-blinking");
 
   if (!isDeleting && charIndex < currentWord.length) {
-    // If condition is true, type the next character
     charIndex++;
     setTimeout(typeEffect, 200);
   } else if (isDeleting && charIndex > 0) {
-    // If condition is true, remove the previous character
     charIndex--;
     setTimeout(typeEffect, 100);
   } else {
-    // If word is deleted then switch to the next word
     isDeleting = !isDeleting;
     dynamicText.classList.remove("stop-blinking");
     wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
